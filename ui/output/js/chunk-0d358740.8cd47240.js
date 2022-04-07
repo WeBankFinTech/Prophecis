852bfcbe36bad404d6b9ee2659afb7672614c385
Alex <382080314@qq.com>
2022-04-07 16:05:46 +0800

    fix(ui): Remove output in UI remove output in UI.

--- ui/output/js/chunk-0d358740.8cd47240.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-0d358740"],{"8cdb":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("article",[t("h1",[e._v("404")]),t("p",[e._v("抱歉，你所访问的页面不存在")])])}],r=(t("b859"),t("2877")),u={},i=Object(r["a"])(u,c,a,!1,null,"43cec9ae",null);n["default"]=i.exports},a17a:function(e,n,t){},b859:function(e,n,t){"use strict";t("a17a")}}]);
-//# sourceMappingURL=chunk-0d358740.8cd47240.js.map
